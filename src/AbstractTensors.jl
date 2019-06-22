14210b84a5794d229e4c3e8c1fd783e759759d09
Michael Reed <18372368+chakravala@users.noreply.github.com>
2019-06-22 08:48:55 -0400

    improved abs, norm, contraction, trigonometric dispatch; fixed #1

--- src/AbstractTensors.jl
+++ src/AbstractTensors.jl
@@ -16,6 +16,7 @@ import LinearAlgebra: dot, cross, norm, UniformScaling
 # parameters accessible from anywhere
 
 Base.@pure vectorspace(::T) where T<:TensorAlgebra{V} where V = V
+Base.@pure ndims(::T) where T<:TensorAlgebra{V} where V = ndims(V)
 
 # universal vector space interopability
 
@@ -38,12 +39,12 @@ end
 
 # extended compatibility interface
 
-export TensorAlgebra, interop, interform, scalar, involute, norm, norm2, unit, even, odd
+export TensorAlgebra, interop, interform, scalar, involute, norm, unit, even, odd
 export ⊗, ⊛, ⊙, ⊠, ⨼, ⨽, ⋆, ⁻¹, ǂ, ₊, ₋, ˣ
 
 # some shared presets
 
-for op ∈ (:(Base.:+),:(Base.:-),:(Base.:*),:⊗,:⊛,:⨼,:⨽,:dot,:cross,:(Base.:|),:(Base.:(==)),:(Base.:<),:(Base.:>),:(Base.:<<),:(Base.:>>),:(Base.:>>>),:(Base.div),:(Base.rem),:(Base.:&),:(Base.:^))
+for op ∈ (:(Base.:+),:(Base.:-),:(Base.:*),:⊗,:⊛,:⨼,:⨽,:dot,:cross,:contraction,:(Base.:|),:(Base.:(==)),:(Base.:<),:(Base.:>),:(Base.:<<),:(Base.:>>),:(Base.:>>>),:(Base.div),:(Base.rem),:(Base.:&),:(Base.:^))
     @eval begin
         @inline $op(a::A,b::B) where {A<:TensorAlgebra,B<:TensorAlgebra} = interop($op,a,b)
         @inline $op(a::A,b::UniformScaling) where A<:TensorAlgebra{V} where V = $op(a,V(b))
@@ -51,13 +52,17 @@ for op ∈ (:(Base.:+),:(Base.:-),:(Base.:*),:⊗,:⊛,:⨼,:⨽,:dot,:cross,:(B
     end
 end
 
+@inline ⋆(t::UniformScaling{T}) where T = T<:Bool ? (t.λ ? 1 : -1) : t.λ
+@inline ⊛(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = scalar(contraction(a,b))
+@inline ⨽(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = contraction(a,b)
+@inline ⨼(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = contraction(b,a)
+@inline Base.:<(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = contraction(b,a)
+@inline Base.:>(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = contraction(a,b)
+@inline Base.:|(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = contraction(a,b)
+
 for op ∈ (:(Base.:+),:(Base.:*))
     @eval $op(t::T) where T<:TensorAlgebra = t
 end
-
-⋆(t::UniformScaling{T}) where T = T<:Bool ? (t.λ ? 1 : -1) : t.λ
-⨽(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = dot(a,b)
-Base.:|(a::TensorAlgebra{V},b::TensorAlgebra{V}) where V = dot(a,b)
 for op ∈ (:|,:!), T ∈ (TensorAlgebra,UniformScaling)
     @eval Base.$op(t::$T) = ⋆(t)
 end
@@ -65,17 +70,56 @@ for op ∈ (:⊙,:⊠)
     @eval function $op end
 end
 for op ∈ (:scalar,:involute,:even)
-    @eval $op(x::T) where T<:Number = x
+    @eval $op(t::T) where T<:Number = t
 end
-odd(x::T) where T<:Number = 0
+odd(::T) where T<:Number = 0
+
+@inline Base.real(t::T) where T<:TensorAlgebra = scalar(t)
+@inline Base.imag(t::T) where T<:TensorAlgebra = t-real(t)
+@inline Base.exp(t::T) where T<:TensorAlgebra = 1+expm1(t)
+@inline Base.log(b,t::T) where T<:TensorAlgebra = log(t)/log(b)
+@inline ^(b::S,t::T) where {S<:Number,T<:TensorAlgebra} = exp(t*log(b))
+@inline ^(b::S,t::T) where {S<:TensorAlgebra{V},T<:TensorAlgebra{V}} where V = exp(t*log(b))
+
+for base ∈ (2,10)
+    fl,fe = (Symbol(:log,base),Symbol(:exp,base))
+    @eval Base.$fl(t::T) where T<:TensorAlgebra = $fl(ℯ)*log(t)
+    @eval Base.$fe(t::T) where T<:TensorAlgebra = exp(log($base)*t)
+end
+
+@inline Base.cos(t::T) where T<:TensorAlgebra{V} where V = cosh(V(I)*t)
+@inline Base.sin(t::T) where T<:TensorAlgebra{V} where V = (i=V(I);sinh(i*t)/i)
+@inline Base.tan(t::T) where T<:TensorAlgebra = sin(t)/cos(t)
+@inline Base.cot(t::T) where T<:TensorAlgebra = cos(t)/sin(t)
+@inline Base.sec(t::T) where T<:TensorAlgebra = inv(cos(t))
+@inline Base.csc(t::T) where T<:TensorAlgebra = inv(sin(t))
+@inline Base.asec(t::T) where T<:TensorAlgebra = acos(inv(t))
+@inline Base.acsc(t::T) where T<:TensorAlgebra = asin(inv(t))
+@inline Base.sech(t::T) where T<:TensorAlgebra = inv(cosh(t))
+@inline Base.csch(t::T) where T<:TensorAlgebra = inv(sinh(t))
+@inline Base.asech(t::T) where T<:TensorAlgebra = acosh(inv(t))
+@inline Base.acsch(t::T) where T<:TensorAlgebra = asinh(inv(t))
+@inline Base.tanh(t::T) where T<:TensorAlgebra = sinh(t)/cosh(t)
+@inline Base.coth(t::T) where T<:TensorAlgebra = cosh(t)/sinh(t)
+@inline Base.asinh(t::T) where T<:TensorAlgebra = log(t+sqrt(1+t^2))
+@inline Base.acosh(t::T) where T<:TensorAlgebra = log(t+sqrt(t^2-1))
+@inline Base.atanh(t::T) where T<:TensorAlgebra = (log(1+t)-log(1-t))/2
+@inline Base.acoth(t::T) where T<:TensorAlgebra = (log(t+1)-log(t-1))/2
+Base.asin(t::T) where T<:TensorAlgebra{V} where V =(i=V(I);-i*log(i*t+sqrt(1-t^2)))
+Base.acos(t::T) where T<:TensorAlgebra{V} where V =(i=V(I);-i*log(t+i*sqrt(1-t^2)))
+Base.atan(t::T) where T<:TensorAlgebra{V} where V =(i=V(I);(-i/2)*(log(1+i*t)-log(1-i*t)))
+Base.acot(t::T) where T<:TensorAlgebra{V} where V =(i=V(I);(-i/2)*(log(t-i)-log(t+i)))
+Base.sinc(t::T) where T<:TensorAlgebra{V} where V = iszero(t) ? one(V) : (x=(1*π)*t;sin(x)/x)
+Base.cosc(t::T) where T<:TensorAlgebra{V} where V = iszero(t) ? zero(V) : (x=(1*π)*t; cos(x)/t - sin(x)/(x*t))
 
 # absolute value norm
 
-@inline Base.abs(t::T) where T<:TensorAlgebra = sqrt(abs(value(dot(t,t))))
-@inline Base.abs2(t::T) where T<:TensorAlgebra = value(dot(t,t))
-@inline norm(t::T) where T<:TensorAlgebra = sqrt(abs(value(dot(t,~t))))
-@inline norm2(t::T) where T<:TensorAlgebra = t*~t
-@inline unit(t::T) where T<:TensorAlgebra = t/abs(t)
+@inline Base.abs(t::T) where T<:TensorAlgebra{V} where V = sqrt(abs2(t))
+@inline Base.abs2(t::T) where T<:TensorAlgebra = (~t)*t
+@inline norm(t::T) where T<:TensorAlgebra = norm(value(t))
+@inline unit(t::T) where T<:TensorAlgebra = t/value(abs(t))
+@inline Base.iszero(t::T) where T<:TensorAlgebra = norm(t) ≈ 0
+@inline Base.isone(t::T) where T<:TensorAlgebra = norm(t) ≈ value(scalar(t)) ≈ 1
 
 # postfix operators
 
